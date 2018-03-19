f296a001908a2f8d5ce405f99e249805ad7e21fd
skardish <kard0019@algonquinlive.com>
2018-03-19 09:21:40 -0400

    Fix image sizing

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/skardish/Desktop/Semester 4/Web Dev 4/ecommerce-pattern-library
- * @version 1521465337339
+ * @version 1521465655781
  */
-const patternManifest_1521465337339 = {
+const patternManifest_1521465655781 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -929,5 +929,5 @@ const patternManifest_1521465337339 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521465337339);
+patternBotIncludes(patternManifest_1521465655781);
 }());
