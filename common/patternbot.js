120e38b8bc99fe73ef37b6d0790071f34ee3255e
skardish <kard0019@algonquinlive.com>
2018-04-23 09:25:11 -0400

    Fixed banner page errors

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/skardish/Desktop/ecommerce-pattern-library
- * @version 1524489508764
+ * @version 1524489888828
  */
-const patternManifest_1524489508764 = {
+const patternManifest_1524489888828 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -921,5 +921,5 @@ const patternManifest_1524489508764 = {
   }
 };
 
-patternBotIncludes(patternManifest_1524489508764);
+patternBotIncludes(patternManifest_1524489888828);
 }());
